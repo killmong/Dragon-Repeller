0b3603c9a017ebf0e114a47d6d472dbd64a09e0a
killmong <88731394+killmong@users.noreply.github.com>
2024-04-14 11:19:37 +0530

    Add files via upload

---
+++ 1sr.js
@@ -0,0 +1,15 @@
+"use strict";
+alert(3+3)
+const id = 1234
+console.log(id) // 123
+let username ="user1"
+let email ="user@email.com"
+let password="password123"
+username = "New York"
+email= "USA@gmail.com"
+password = "jdjgadlg"
+let accountstate;
+ 
+
+console.table([email,username,password])
+// 
---
+++ second.js
@@ -0,0 +1,38 @@
+// console.log("Hrll") //code readability should ne high 
+// let name ="gorab"
+// let age = 30
+// let isloggedin = false
+
+// // number = 2 to power 53
+
+// //bigint for number out of range of int
+// //string = ""
+// // boolean - T or F
+// // null - a special data type used to indicate the absence of a value or a variable that has not been assigned a value
+// //undefined
+// // symbol = unique
+
+// //object  - collection of property and value pair
+// console.log(typeof(name))
+// console.log(typeof(null))
+// console.log(typeof (undefined))
+
+// let score = "null"
+// console.log(typeof(score))
+// let valueinnum = Number(score)
+
+// console.log(typeof(valueinnum ))
+
+// console.log(valueinnum )
+// 33 > 33
+// 333abssd >>>> NaN (type number)
+// true > 1 && false >0
+
+let islogged = "dhdsiin"
+let boollog = Boolean(islogged)
+console.log(boollog)
+
+let num = 34
+let str = String(num)
+console.log(str)
+console.log(typeof(str))
